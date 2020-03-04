8290e25134708dc43e191488760c553feab2938b
sflores24 <flores.salvador@gmail.com>
2020-03-04 17:52:58 -0600

    user common

---
+++ src/main/java/mx/com/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplication.java
@@ -0,0 +1,10 @@
+package mx.com.springboot.app.usuarios.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootServicioUsuariosCommonsApplication {
+}
